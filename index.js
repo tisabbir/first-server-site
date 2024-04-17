62363f3a754b5503f91f21501e334d502225ee6b
TI Sabbir <tisabbir0@gmail.com>
2024-04-18 00:14:51 +0600

    cors access problem solved

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,7 @@ const users = [
     { id: 3, name : 'labana', email : "labana@gmail.com"},
 ]
 
-
+app.use(cors())
 
 
 app.get('/', (req, res) => {
